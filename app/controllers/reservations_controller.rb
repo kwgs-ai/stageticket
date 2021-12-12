d4667c73d30c69f643fcdba5a79f342f27ba5c3b
Ai Kawagishi <ne201062@senshu-u.jp>
2021-12-12 12:35:58 +0000

    ユーザーの舞台予約

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -4,6 +4,15 @@ class ReservationsController < ApplicationController
     @reservation = Reservation.new
   end
   def create
+    @stage = Stage.find(params[:stage_id])
+    @reservation = Reservation.new()
+    @reservation.useraccount_id = session[:user_id]
+    @reservation.stages << @stage
+    if @reservation.save!
+      redirect_to :root, notice: "登録しました"
+    else
+      render "new"
+    end
 
   end
 end

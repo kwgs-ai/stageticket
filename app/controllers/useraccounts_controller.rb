eb63c1fc295f38776298d9efc1881f85b11cc2ff
Ai Kawagishi <ne201062@senshu-u.jp>
2021-12-12 13:55:47 +0000

    ユーザー編集

--- app/controllers/useraccounts_controller.rb
+++ app/controllers/useraccounts_controller.rb
@@ -1,11 +1,14 @@
 class UseraccountsController < ApplicationController
   before_action :user_login_required, only: [:index]
+
   def index
 
   end
+
   def new
     @user = Useraccount.new
   end
+
   def create
     @user = Useraccount.new(params[:useraccount])
     if @user.save
@@ -14,12 +17,22 @@ class UseraccountsController < ApplicationController
       render "new"
     end
   end
+
   def edit
-
+    @user = Useraccount.find(params[:id])
   end
+
   def update
+    @user = Useraccount.find(params[:id])
+    @user.assign_attributes(params[:useraccount])
+    if @user.save
+      redirect_to @user, notice: "会員情報を更新しました。"
+    else
+      render "edit"
+    end
 
   end
+
   def show
     @user = current_user
   end

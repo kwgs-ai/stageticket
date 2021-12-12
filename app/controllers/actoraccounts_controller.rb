43431d2c726d58c0ace334e94ae8d52f4ba1f5df
Ai Kawagishi <ne201062@senshu-u.jp>
2021-12-12 13:19:04 +0000

    公演団体の新規登録

--- app/controllers/actoraccounts_controller.rb
+++ app/controllers/actoraccounts_controller.rb
@@ -2,11 +2,24 @@ class ActoraccountsController < ApplicationController
   before_action :actor_login_required, only: [:index]
 
   def index
-
+    @actor = Actoraccount.new
   end
 
   def show
     @actor = current_actor
+  end
+
+  def new
+    @actor = Actoraccount.new
+  end
+
+  def create
+    @actor = Actoraccount.new(params[:actoraccount])
+    if @actor.save
+      redirect_to :root, notice: "登録しました"
+    else
+      render "new"
+    end
   end
 
 end

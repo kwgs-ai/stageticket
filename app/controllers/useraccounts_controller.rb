3895e8867eb8ab91df9fa5faabe1c822e61a8260
Ai Kawagishi <ne201062@senshu-u.jp>
2021-12-12 12:59:27 +0000

    ユーザーの新規登録

--- app/controllers/useraccounts_controller.rb
+++ app/controllers/useraccounts_controller.rb
@@ -3,7 +3,23 @@ class UseraccountsController < ApplicationController
   def index
 
   end
+  def new
+    @user = Useraccount.new
+  end
+  def create
+    @user = Useraccount.new(params[:useraccount])
+    if @user.save
+      redirect_to :root, notice: "登録しました"
+    else
+      render "new"
+    end
+  end
+  def edit
 
+  end
+  def update
+
+  end
   def show
     @user = current_user
   end

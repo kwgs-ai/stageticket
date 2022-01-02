46f48d6f9f1f4185e05a7328b2996747ab21998e
Ai Kawagishi <ne201062@senshu-u.jp>
2022-01-02 13:53:32 +0000

    席修正

--- db/seeds/development/seats.rb
+++ db/seeds/development/seats.rb
@@ -1,4 +1,4 @@
-types = ['S', 'A', 'B']
+types = %w[S A A B B]
 costs = [7000, 5000, 1000]
 
 1.upto(26) do |idx|
@@ -8,7 +8,7 @@ costs = [7000, 5000, 1000]
   1.upto(30) do |idx|
     Seat.create(
       stage_id: stage.id,
-      seat_type: (types[idx % 3]).to_s,
+      seat_type: (types[idx % 5]).to_s,
       seat_prise: costs[idx % 3]
     )
   end

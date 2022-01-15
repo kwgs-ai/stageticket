e4fc13524a4db76e7a859113d378ef7252107909
Ai Kawagishi <ne201062@senshu-u.jp>
2022-01-15 20:39:34 +0000

    ログイン画面デザイン変更

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -7,10 +7,12 @@ class Actor < ApplicationRecord
   validates :password, presence: { if: :current_password },
             length: { minimum: 4, maximum: 16, allow_blank: true }
   validates :name, presence: true,
-            length: { minimum: 2, maximum: 20, allow_blank: true }
-            # uniqueness: true
+            length: { minimum: 2, maximum: 20, allow_blank: true },
+            format: {  with: /\A[0-9A-Za-z]*\z/, allow_blank: true },
+            uniqueness: true
   validates :login_name, presence: true,
-            length: { minimum: 4, maximum: 16, allow_blank: true }
-            # uniqueness: true
+            length: { minimum: 4, maximum: 16, allow_blank: true },
+            format: {  with: /\A[0-9A-Za-z]*\z/, allow_blank: true },
+            uniqueness: true
 
 end

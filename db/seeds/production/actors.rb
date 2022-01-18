70169d372a0b2cc1dd30751ae97e5aba784813f3
Ai Kawagishi <ne201062@senshu-u.jp>
2022-01-18 11:03:56 +0000

    heroku seed誤字脱字修正

--- db/seeds/production/actors.rb
+++ db/seeds/production/actors.rb
@@ -2,10 +2,10 @@ names = %w[春秋 劇団四季 オペラ団 劇団青い鳥 劇団NS オペラ�
            劇団ネット]
 lnames = %w[haruaki gekidannshiki operadann gekidannaoitori gekidannNS operaza gekidannharuka gekidannseseragi gekidannao gekidannnetto]
 0.upto(9) do |idx|
-  Actor.create!(
-    name: names[idx],
-    login_name: lnames[idx],
-    password: '1111',
-    password_confirmation: '1111'
-  )
+    Actor.create!(
+      name: names[idx],
+      login_name: lnames[idx],
+      password: '1111',
+      password_confirmation: '1111'
+    )
 end

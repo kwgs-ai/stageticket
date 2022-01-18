2484388f95a1de9f0a0ddcdefb30b062094cfdc5
Ai Kawagishi <ne201062@senshu-u.jp>
2022-01-18 11:02:33 +0000

    heroku seed誤字脱字修正

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -5,18 +5,18 @@ class Actor < ApplicationRecord
   attr_accessor :current_password
 
   validates :password, presence: { if: :current_password },
-            length: { minimum: 4, maximum: 16, allow_blank: true },
-            format: {  with: /\A[0-9A-Za-z]*\z/, allow_blank: true,
-                       message: :invalid_actor_pass
+                       length: { minimum: 4, maximum: 16, allow_blank: true },
+                       format: {  with: /\A[0-9A-Za-z]*\z/, allow_blank: true,
+                                  message: :invalid_actor_pass
             }
   validates :name, presence: true,
-            length: { minimum: 2, maximum: 20, allow_blank: true },
-            uniqueness: true
+                   length: { minimum: 2, maximum: 20, allow_blank: true },
+                   uniqueness: true
   validates :login_name, presence: true,
-            length: { minimum: 4, maximum: 16, allow_blank: true },
-            format: {  with: /\A[0-9A-Za-z]*\z/, allow_blank: true,
-                       message: :invalid_actor_login
+                         length: { minimum: 4, maximum: 16, allow_blank: true },
+                         format: {  with: /\A[0-9A-Za-z]*\z/, allow_blank: true,
+                                    message: :invalid_actor_login
             },
-            uniqueness: true
+                         uniqueness: true
 
 end
--- db/seeds/development/actors.rb
+++ db/seeds/development/actors.rb
@@ -1,6 +1,6 @@
 names = %w[春秋 劇団四季 オペラ団 劇団青い鳥 劇団NS オペラ座 劇団はるか 劇団せせらぎ 劇団あお
            劇団ネット]
-lnames = %w[haruaki gekidannshiki operadann gekidannaoitori gekidannNS operaza gekidannharuka gekidannseseragi gekidannao gekidannnetto ]
+lnames = %w[haruaki gekidannshiki operadann gekidannaoitori gekidannNS operaza gekidannharuka gekidannseseragi gekidannao gekidannnetto]
 0.upto(9) do |idx|
     Actor.create!(
       name: names[idx],
